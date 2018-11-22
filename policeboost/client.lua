399cdde934e7fb85097a9c7ac38c9705e0e6d2c1
timnboys <timnboys@users.noreply.github.com>
2018-11-22 16:17:51 -0500

    Update client.lua

--- policeboost/client.lua
+++ policeboost/client.lua
@@ -5,7 +5,7 @@ Citizen.CreateThread(function()
         local veh = GetVehiclePedIsIn(ped, false)
         if veh ~= nil then
 	    local job = exports.esx_policejob:getJob()
-            if GetVehicleClass(veh) == 18 and job ~= nil and job == "police" then
+            if job ~= nil and job == "police" then
 		SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(ped, false), 40.0)                
             end
         end
